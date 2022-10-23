59fe3d1cd76299ba2e61fd884c1d685e89aa21e2
hikikones <hikikones@gmail.com>
2022-10-23 19:50:01 +0200

    update parallel example

--- examples/parallel.rs
+++ examples/parallel.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_sequential_actions::*;
 
-use shared::{actions::*, bootstrap::*};
+use shared::{actions::*, bootstrap::*, extensions::FromLookExt};
 
 fn main() {
     App::new()
@@ -12,28 +12,39 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands, camera_q: Query<Entity, With<CameraMain>>) {
+fn setup(mut commands: Commands, camera_q: Query<Entity, With<CameraPivot>>) {
     let agent = commands.spawn_agent(Vec3::ZERO, Quat::IDENTITY);
-
-    let camera = camera_q.single();
+    let camera_pivot = camera_q.single();
 
     commands
         .actions(agent)
+        .config(AddConfig {
+            order: AddOrder::Back,
+            start: false,
+            repeat: Repeat::Forever,
+        })
         .add(WaitAction::new(1.0))
         .add_many(
             ExecutionMode::Parallel,
             [
                 MoveAction::new(MoveConfig {
-                    target: Vec3::X,
+                    target: Vec3::X * 3.0,
                     speed: 4.0,
                     rotate: false,
                 })
                 .into_boxed(),
                 RotateAction::new(RotateConfig {
                     target: RotateType::Look(Vec3::X),
-                    speed: 0.5,
+                    speed: std::f32::consts::FRAC_PI_2,
                 })
                 .into_boxed(),
+                LerpAction::new(LerpConfig {
+                    target: camera_pivot,
+                    lerp_type: LerpType::Rotation(Quat::from_look(-Vec3::X, Vec3::Y)),
+                    duration: 2.0,
+                })
+                .into_boxed(),
+                WaitAction::new(5.0).into_boxed(),
             ]
             .into_iter(),
         )
@@ -42,17 +53,25 @@ fn setup(mut commands: Commands, camera_q: Query<Entity, With<CameraMain>>) {
             ExecutionMode::Parallel,
             [
                 MoveAction::new(MoveConfig {
-                    target: -Vec3::X,
-                    speed: 4.0,
+                    target: -Vec3::X * 3.0,
+                    speed: 1.0,
                     rotate: false,
                 })
                 .into_boxed(),
                 RotateAction::new(RotateConfig {
                     target: RotateType::Look(-Vec3::X),
-                    speed: 0.5,
+                    speed: std::f32::consts::FRAC_PI_2,
                 })
                 .into_boxed(),
+                LerpAction::new(LerpConfig {
+                    target: camera_pivot,
+                    lerp_type: LerpType::Rotation(Quat::from_look(Vec3::X, Vec3::Y)),
+                    duration: 3.0,
+                })
+                .into_boxed(),
+                WaitAction::new(1.0).into_boxed(),
             ]
             .into_iter(),
-        );
+        )
+        .next();
 }
