ca366d6cb1eb673737ed889cad8d0b3df568d2e6
hikikones <hikikones@gmail.com>
2022-10-25 00:23:53 +0200

    run wait action of new entities

--- examples/shared/src/actions/wait_action.rs
+++ examples/shared/src/actions/wait_action.rs
@@ -16,6 +16,7 @@ where
     F: IntoValue<f32>,
 {
     duration: F,
+    entity: Option<Entity>,
     current: Option<f32>,
 }
 
@@ -26,6 +27,7 @@ where
     pub fn new(seconds: F) -> Self {
         Self {
             duration: seconds,
+            entity: None,
             current: None,
         }
     }
@@ -37,27 +39,50 @@ where
 {
     fn on_start(&mut self, agent: Entity, world: &mut World, _commands: &mut ActionCommands) {
         let duration = self.current.take().unwrap_or(self.duration.value());
-        world.entity_mut(agent).insert(Wait(duration));
+        self.entity = Some(
+            world
+                .spawn()
+                .insert_bundle(WaitBundle {
+                    wait: Wait(duration),
+                    agent: Agent(agent),
+                })
+                .id(),
+        );
     }
 
-    fn on_stop(&mut self, agent: Entity, world: &mut World, reason: StopReason) {
-        let wait = world.entity_mut(agent).remove::<Wait>();
+    fn on_stop(&mut self, _agent: Entity, world: &mut World, reason: StopReason) {
+        let entity = self.entity.take().unwrap();
 
         if let StopReason::Paused = reason {
-            self.current = Some(wait.unwrap().0);
+            self.current = Some(world.get::<Wait>(entity).unwrap().0);
         }
+
+        world.despawn(entity);
     }
+}
+
+#[derive(Bundle)]
+struct WaitBundle {
+    wait: Wait,
+    agent: Agent,
 }
 
 #[derive(Component)]
 struct Wait(f32);
 
-fn wait(mut wait_q: Query<(&mut Wait, &mut ActionFinished)>, time: Res<Time>) {
-    for (mut wait, mut finished) in wait_q.iter_mut() {
+#[derive(Component)]
+struct Agent(Entity);
+
+fn wait(
+    mut wait_q: Query<(&mut Wait, &Agent)>,
+    mut finished_q: Query<&mut ActionFinished>,
+    time: Res<Time>,
+) {
+    for (mut wait, agent) in wait_q.iter_mut() {
         wait.0 -= time.delta_seconds();
 
         if wait.0 <= 0.0 {
-            finished.confirm_and_reset();
+            finished_q.get_mut(agent.0).unwrap().confirm_and_reset();
         }
     }
 }
