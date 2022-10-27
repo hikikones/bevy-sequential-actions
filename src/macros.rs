23164c42d04f9780153290e4ea78048f7c93ede7
hikikones <hikikones@gmail.com>
2022-10-27 23:59:31 +0200

    docs

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,28 @@
+/// Helper macro for creating a collection of boxed actions.
+///
+/// ```rust,no_run
+/// # use bevy::prelude::*;
+/// # use bevy_sequential_actions::*;
+/// # use shared::actions::*;
+/// #
+/// fn setup(mut commands: Commands) {
+/// #   let agent = commands.spawn_bundle(ActionsBundle::default()).id();
+/// #
+///     commands
+///         .actions(agent)
+///         .add_many(
+///             ExecutionMode::Parallel,
+///             actions![
+///                 QuitAction,
+///                 DespawnAction,
+///                 WaitAction::new(1.0),
+///                 |agent: Entity, world: &mut World, commands: &mut ActionCommands | {
+///                     // on_start
+///                 },
+///             ]
+///         );
+/// }
+/// ```
 #[macro_export]
 macro_rules! actions {
     ( $( $x:expr ),* $(,)? ) => {
