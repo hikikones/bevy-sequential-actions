0df38fd8e314a3982575550632994a1343ea4d34
hikikones <hikikones@gmail.com>
2022-11-04 23:56:33 +0100

    simplify warning doc

--- src/lib.rs
+++ src/lib.rs
@@ -147,8 +147,7 @@ fn wait_system(mut wait_q: Query<(&mut Wait, &mut ActionFinished)>, time: Res<Ti
 
 One thing to keep in mind is that you should not modify actions using [`World`] inside the [`Action`] trait.
 We cannot borrow a mutable action from an `agent` while also passing a mutable world to it.
-And so, the action is detached from an `agent` when the trait methods are called.
-Since an `agent` cannot hold an action while inside the [`Action`] trait,
+Since an action is detached from an `agent` when the trait methods are called,
 the logic for advancing the action queue will not work properly.
 
 This is why [`ActionCommands`] was created, so you can modify actions inside the [`Action`] trait in a deferred way.
