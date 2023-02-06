8bc8c98b4d72e113114251517ba19ebe7fbe9815
hikikones <hikikones@gmail.com>
2023-02-06 02:34:32 +0100

    fix check_actions

--- src/plugin.rs
+++ src/plugin.rs
@@ -59,10 +59,10 @@ fn check_actions(
         if let Some((current_action, _)) = &current_action.0 {
             let finished_count = finished.total();
             let active_count = match current_action {
-                ActionType::Single(_) => todo!(),
-                ActionType::Parallel(_) => todo!(),
-                ActionType::Linked(_, _) => todo!(),
-            };
+                ActionType::Single(_) => 1,
+                ActionType::Parallel(actions) => actions.len(),
+                ActionType::Linked(_, _) => 1,
+            } as u32;
 
             match finished_count.cmp(&active_count) {
                 Ordering::Less => {}
