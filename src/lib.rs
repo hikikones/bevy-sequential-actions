be75cfcfb5508b1092f1aff003c233330f8abb4e
hikikones <hikikones@gmail.com>
2024-11-06 14:12:31 +0100

    same em for rust doc

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ that simplifies the process of running various actions in a specific sequence.
 
 <figure>
     <img src="https://github.com/user-attachments/assets/66b5b15e-96af-47bd-9371-eee8809d1294"/>
-    <figcaption>An entity with a queue of repeating actions</figcaption>
+    <p><em>An entity with a queue of repeating actions</em></p>
 </figure>
 
 </div>
